b57399fe1ae44b79cf9e2f05b8c3496aadfa9dbb
Ambrose Lai <alairon@outlook.com>
2020-10-26 11:58:47 -0400

    Reduced main function code size

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ require('dotenv').config();
 import config = require('./config.json');
 import Quests = require('./plugins/UQManager/quests');
 import Casino = require('./plugins/UQManager/casino');
-import Fuse = require('fuse.js');
 
 import Discord = require('discord.js');
 const client = new Discord.Client();
@@ -33,64 +32,97 @@ client.on('message', async message => {
   if (message.author.bot) return;
 
   /* Xiera Responses */
-  // Direct Messages
   switch(message.channel.type){
     case 'dm': 
       // Direct Message to the bot
       console.log (`${message.author.username} via DM [${time}]: ${message}`);
-      if (token.test(message.content)) {
-        message.channel.send(`Hi ${message.author.username}, just letting you know that you won't need to flag me down when we're talking through a DM :smile:`);
-        console.log('Remove !xiera, then read command');
+      let content: string = message.content;
+
+      if (token.test(message.content) && message.mentions.has(client.user)){
+        message.channel.send(`<@${message.author.id}>, I can't believe someone would try to flag *and* mention me at the same time! Arrghh!\nSorry for that outburst. Anyways, you don't need to use the flag *or* mention me while we're using DMs.`);
+        break;
+      } else if (message.mentions.has(client.user)){
+        message.channel.send(`Hey! You don't need to mention me when we're talking through a DM!`);
+        //Attempt to remove the mention
+        try {
+          content = message.content.match(/(?<=>\s?).*/mi)[0];
+        }
+        catch(err){
+        }
+      } else if (token.test(message.content)) {
+        message.channel.send(`Hello! Friendly Xiera here letting you know that you won't need to use the flag when we're talking through a DM.`);
+        //Attempt to remove the flag
+        try{
+          content = message.content.match(/(?<=x!\s?).*/mi)[0];
+        }
+        catch(err){
+        }
       }
-      else if (/!test/mi.test(message.content)){
-        message.channel.send('Someone requested I run a quick test. Please check the visiphone terminal for details.');
-        quests.displayEvents();
-        casino.displayEvents();
+
+      //Divide the string into tags
+      let command: Array<string>;
+      
+      if (content){
+        command = content.split(' ');
       }
-      else if (/uq/mi.test(message.content)){
-        let events = quests.events;
-        console.log(events);
+      else{
+        command = [];
       }
-      else if (/casino/mi.test(message.content)){
-        let events = casino.events;
-        console.log(events);
-      }
-      else {
-        console.log('Read command without !xiera');
+
+      switch (command[0]) {
+        case 'uq':
+          const uqRes = await quests.findEvent(content);
+          message.channel.send(uqRes);
+          break;
+        case 'casino':
+          const casinoRes = await casino.displayEvents();
+          message.channel.send(casinoRes);
+          break;
+        case 'info':
+          message.channel.send(`I'm currently working on that, so I don't have anything to share right now`);
+          break;
+        default: 
+          message.channel.send(`Hello ${message.author.username}!\nI can currently tell you what scheduled events are happening on the Oracle Fleet!\nJust let me know if you want info about upcoming urgent quests (\`uq\`), or what boosts are available at the casino (\`casino\`).`);
       }
       break;
     case 'text':
-      // Message on a server channel
+      // Message on a server's text channel
+      if (message.mentions.has(client.user)){ console.log(message)}
+
+      // Respond only if the flag is present
       if (token.test(message.content)){
-        const command = message.content.split(' ');
+        //Attempt to remove the flag
+        let content: string;
+        try{
+          content = message.content.match(/[^(^x!\s?)].*/mi)[0];
+        }
+        catch(err){
+        }
+
+        //Divide the string into tags
+        let command: Array<string>;
+        
+        if (content){
+          command = content.split(' ');
+        }
+        else{
+          command = [];
+        }
+
         console.log (`${message.author.username} via ${message.guild.name} [${time}]: ${command}`);
-        switch (command[1]) {
+        switch (command[0]) {
           case 'uq':
-            if (command[2]){
-              message.channel.send(`${command[2]}?`);
-            } 
-            console.log(quests.getEvents());
+            const uqRes = await quests.findEvent(content);
+            message.channel.send(uqRes);
             break;
           case 'casino':
-            if (command[2]){
-              message.channel.send(`${command[2]}?`);
-            }
-            console.log(casino.getEvents());
+            const casinoRes = await casino.displayEvents();
+            message.channel.send(casinoRes);
             break;
           case 'info':
-            const details = command[2];
-
-            if (details){
-              let userMsg = await message.channel.send(`You're looking for '${details}'? Sure thing! I'll just look it up for you, won't take long.`);
-              console.log(details);
-              userMsg.edit('Done!');
-            }
-            else {
-              message.channel.send(`I can't seem to find anything about this in the database. I'll ask Casra if he knows anything about it.`);
-            }
             break;
           default: 
-            message.channel.send(`Hello ${message.author.username}!\nI can currently tell you what events are happening around ARKS based on what Casara sent me.\nJust let me know if you want info about urgent quests (\`uq\`), or what's going on at the casino (\`casino\`).`);
+          message.channel.send(`Hello ${message.author.username}!\nI can currently tell you what scheduled events are happening on the Oracle Fleet!\nJust let me know if you want info about upcoming urgent quests (\`uq\`), or what boosts are available at the casino (\`casino\`).`);
         }
       }
       break;
