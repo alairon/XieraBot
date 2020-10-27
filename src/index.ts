a9d7d02651c2fdfb46229f1c3c2ec070ee3bd93f
Ambrose Lai <alairon@outlook.com>
2020-10-26 23:15:26 -0400

    Changed config file to be (re)read on demand

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
 require('dotenv').config();
-import config = require('./config.json');
-import Quests = require('./plugins/UQManager/quests');
-import Casino = require('./plugins/UQManager/casino');
+import fs = require('fs');
+import path = require('path');
+import Quests = require('./plugins/EventManager/quests');
+import Casino = require('./plugins/EventManager/casino');
 
 import Discord = require('discord.js');
 const client = new Discord.Client();
+
+// Configure and initialize
+const config: XieraConfig = readConfig(path.join(__dirname,'xiera.json'));
 const token = new RegExp (config.xiera.token, config.xiera.flags);
-
-let quests = new Quests.Events();
-quests.initEvents();
-quests.setEvents();
-
-let casino = new Casino.Events();
-casino.initEvents();
-casino.setEvents();
+const quests = new Quests.Events(config.calendar.quest.url, config.calendar.quest.refreshInterval);
+const casino = new Casino.Events(config.calendar.casino.url, config.calendar.casino.refreshInterval);
 
 // Startup. Run all startup functions once the Discord client is ready
 client.once('ready', () => {
@@ -71,28 +69,38 @@ client.on('message', async message => {
 
       switch (command[0]) {
         case 'uq':
+        case 'q':
+        case 'quests':
+        case 'events':
           const uqRes = await quests.findEvent(content);
           message.channel.send(uqRes);
           break;
         case 'casino':
+        case 'c':
           const casinoRes = await casino.displayEvents();
           message.channel.send(casinoRes);
           break;
         case 'info':
-          message.channel.send(`I'm currently working on that, so I don't have anything to share right now`);
+        case 'i':
+          message.channel.send(`This function isn't quite ready yet, so I don't really have anything to share right now.`);
           break;
         default: 
           message.channel.send(`Hello ${message.author.username}!\nI can currently tell you what scheduled events are happening on the Oracle Fleet!\nJust let me know if you want info about upcoming urgent quests (\`uq\`), or what boosts are available at the casino (\`casino\`).`);
       }
-      break;
     case 'text':
       // Message on a server's text channel
-      if (message.mentions.has(client.user)){ console.log(message)}
-
-      // Respond only if the flag is present
-      if (token.test(message.content)){
-        //Attempt to remove the flag
+      if (token.test(message.content) || message.mentions.has(client.user)){
         let content: string;
+        //Attempt to remove any mentions if it was a mention
+        if (message.mentions.has(client.user)){
+          //Attempt to remove any mentions
+          try {
+            content = message.content.match(/(?<=>\s?).*/mi)[0];
+          }
+          catch(err){
+          }
+        }
+        //Attempt to remove the flag
         try{
           content = message.content.match(/[^(^x!\s?)].*/mi)[0];
         }
@@ -112,14 +120,18 @@ client.on('message', async message => {
         console.log (`${message.author.username} via ${message.guild.name} [${time}]: ${command}`);
         switch (command[0]) {
           case 'uq':
+          case 'u':
+          case 'events':
             const uqRes = await quests.findEvent(content);
             message.channel.send(uqRes);
             break;
           case 'casino':
+          case 'c':
             const casinoRes = await casino.displayEvents();
             message.channel.send(casinoRes);
             break;
           case 'info':
+          case 'i':
             break;
           default: 
           message.channel.send(`Hello ${message.author.username}!\nI can currently tell you what scheduled events are happening on the Oracle Fleet!\nJust let me know if you want info about upcoming urgent quests (\`uq\`), or what boosts are available at the casino (\`casino\`).`);
@@ -134,3 +146,25 @@ client.on('message', async message => {
 
 //Logs into the client using the Discord Client Key
 client.login(process.env.CLIENTKEY);
+
+function readConfig(configDir: string): XieraConfig{
+  const config = fs.readFileSync(configDir, 'utf8');
+  return JSON.parse(config);
+}
+
+interface XieraConfig {
+  "xiera": {
+    token: string,
+    flags: string 
+  },
+  "calendar": {
+    "quest": {
+      refreshInterval: number,
+      url: string
+    },
+    "casino": {
+      refreshInterval: number,
+      url: string
+    }
+  }
+}
