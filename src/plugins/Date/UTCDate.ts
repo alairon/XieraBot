e6de05c18d2f354821a240c38756ce39b9fcce67
Ambrose Lai <alairon@outlook.com>
2020-10-28 23:47:51 -0400

    Modified directory for better readability

--- src/plugins/Date/UTCDate.ts
+++
@@ -1,6 +0,0 @@
-export class UTCDate{
-  public getUTCString(date: Date): String{
-    const UTCDate: Date = new Date(Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds(), date.getUTCMilliseconds()));
-    return UTCDate.toISOString();
-  }
-}
