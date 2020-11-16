3d46960ac523c2a1ca8cfe9b213aa7ba70747ef1
Ambrose Lai <alairon@outlook.com>
2020-11-16 17:12:58 -0500

    Allows easier token changes via the config file

--- xiera/components/Core/Token/TokenManager.ts
+++ xiera/components/Core/Token/TokenManager.ts
@@ -1,8 +1,13 @@
 export class TokenManager{
   token: RegExp;
+  commands: RegExp;
 
+  // Sets up the token
   constructor(token: string, flags: string){
-    this.token = new RegExp(token, flags);
+    // Token at the beginning of the message, with an optional space
+    this.token = new RegExp(`^${token}\\s?`, flags); 
+    // The text after the token.
+    this.commands = new RegExp(`(?<=${token}\\s?).*`, flags);
   }
 
   // Returns whether the token exists in the string
@@ -17,7 +22,7 @@ export class TokenManager{
   
   // Removes the flag.
   public removeToken(input: string): string{
-    let tokenlessString: RegExpMatchArray = input.match(this.token);
+    let tokenlessString: RegExpMatchArray = input.match(this.commands);
     let resultString: string;
     try{
       resultString = tokenlessString[0];
