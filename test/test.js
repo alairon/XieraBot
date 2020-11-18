adc03ed7a43ea4bda57a95c5482b70564b7b5ea1
Ambrose Lai <alairon@outlook.com>
2020-11-18 16:52:50 -0500

    Skips on invalid array lengths

--- test/test.js
+++ test/test.js
@@ -1,10 +1,12 @@
 //require('./components/events/events.test');
 
 const toTest = [
+  // [Name of function to be tested, Testing file location]
   ["TimeStrings", "./components/Core/Date/TimeStrings.test"]
 ];
 
 for (const idx in toTest){
+  if (toTest[idx].length < 2) continue; // Skips if there are fewer than two keys in an index
   console.log(`Testing: ${toTest[idx][0]}`);
   require(toTest[idx][1]);
 }
