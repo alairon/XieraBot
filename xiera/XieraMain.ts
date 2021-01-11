bf0dee5f7796f51161b93305e8718e90cdb1a901
Ambrose Lai <alairon@outlook.com>
2021-01-10 22:45:40 -0500

    Decapitalized client

--- xiera/XieraMain.ts
+++ xiera/XieraMain.ts
@@ -40,7 +40,7 @@ let config: XieraConfig = readConfig(path.join(__dirname, 'xiera.json'));
 let XieraStrings: XieraString = readStrings(path.join(__dirname, 'XieraStrings.json'));
 
 // Initialize the Discord Client
-const Client = new Discord.Client();
+const client = new Discord.Client();
 const Token = new TokenManager.TokenManager(config.xiera.token, config.xiera.flags);
 const Event = new Events.Events();
 const DailyCraft = new DailyCrafting();
@@ -58,50 +58,51 @@ const HelpMessage = XieraStrings.client.message.usage;
  * DISCORD CLIENT FUNCTIONS
  */
 // Log into Discord using the Discord client key
-Client.login(process.env.CLIENTKEY).catch((err) => { console.error(XieraStrings.client.login.error + `\n${err}`) });
+client.login(process.env.CLIENTKEY).catch((err) => { console.error(XieraStrings.client.login.error + `\n${err}`) });
 
 // When Xiera is finished loading
-Client.once('ready', () => {
+client.once('ready', () => {
   console.log(XieraStrings.client.on.ready);
+  client.user.setActivity('over the ARKS', {type: 'WATCHING'});
 });
 
 // Being rate limited
-Client.on('rateLimit', (rateData) => {
+client.on('rateLimit', (rateData) => {
   console.log(XieraStrings.client.on.rateLimit + `\n${rateData}`);
 });
 
 // Upon receiving a warning
-Client.on('warn', (msg) => {
+client.on('warn', (msg) => {
   console.log(XieraStrings.client.on.warn + `\n${msg}`);
 });
 
 // Upon receiving an error
-Client.on('error', (err) => {
+client.on('error', (err) => {
   console.error(XieraStrings.client.on.error + `\n${err}`);
 });
 
 // Log any websocket errors
-Client.on('shardError', (err) => {
+client.on('shardError', (err) => {
   console.error(XieraStrings.client.on.shardError + `\n${err}`);
 });
 
 // Xiera attempting to reconnect to Discord
-Client.on('shardReconnecting', () => {
+client.on('shardReconnecting', () => {
   console.log(XieraStrings.client.on.shardReconnecting);
 });
 
 // When reconnected to Discord
-Client.on('shardResume', () => {
+client.on('shardResume', () => {
   console.log(XieraStrings.client.on.shardResume);
 });
 
 // Catch unhandled rejections from async functions
-Client.on('unhandledRejection', (err) => {
+client.on('unhandledRejection', (err) => {
   console.error(XieraStrings.client.on.unhandledRejection + `\n${err}`);
 });
 
 // Xiera's bahaviour upon receiving a message
-Client.on('message', async (message) => {
+client.on('message', async (message) => {
   // Stop Xiera from responding to herself (or any other bots)
   if (message.author.bot) return;
 
